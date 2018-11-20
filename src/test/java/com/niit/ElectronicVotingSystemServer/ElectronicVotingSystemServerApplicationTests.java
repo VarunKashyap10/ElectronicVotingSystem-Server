53b784a99773c938aa6823ccb8dd5d41a80a24df
Varun Kashyap <Varun Kashyap>
2018-11-20 13:37:05 +0530

    Noerror save

--- src/test/java/com/niit/ElectronicVotingSystemServer/ElectronicVotingSystemServerApplicationTests.java
+++ src/test/java/com/niit/ElectronicVotingSystemServer/ElectronicVotingSystemServerApplicationTests.java
@@ -139,8 +139,8 @@ public class ElectronicVotingSystemServerApplicationTests {
 		AdministratorImpl ad=new AdministratorImpl(ele,par,cand,appd);
 		ArrayList<ApplicationBean> arr2=ad.viewAllAdminPendingApplications();
 		//System.out.println(arrT.remove(1).toString());
-		//assertEquals("View Pending Applications Failed",arrT,arr2);
-		assertNotEquals(arr, arr2);
+		assertEquals("View Pending Applications Failed",arrT,arr2);
+		//assertNotEquals(arr, arr2);
 	}
 	
 
